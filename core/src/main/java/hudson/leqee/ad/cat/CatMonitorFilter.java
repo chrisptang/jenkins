1d918e8400aea75ec14552f981489790821a6fae
PENG TANG <pengtang@PENGs-MacBook-Pro.local>
2020-11-18 09:28:59 +0800

    added URL monitor

--- core/src/main/java/hudson/leqee/ad/cat/CatMonitorFilter.java
+++ core/src/main/java/hudson/leqee/ad/cat/CatMonitorFilter.java
@@ -3,9 +3,11 @@ package hudson.leqee.ad.cat;
 import com.dianping.cat.Cat;
 import com.dianping.cat.configuration.client.entity.ClientConfigProperty;
 import com.dianping.cat.log.CatLogger;
+import com.dianping.cat.servlet.CatFilter;
 import com.leqee.boot.autoconfiguration.common.LogPathUtil;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.logging.Logger;
 
@@ -14,6 +16,10 @@ public class CatMonitorFilter implements Filter {
     private static final Logger LOGGER = Logger.getLogger(CatMonitorFilter.class.getName());
 
     private static final String DEFAULT_SERVER = "10.0.16.134";
+
+    private static final CatFilter CAT_FILTER = new CatFilter();
+
+    private static final String REG = ".*(\\.png|\\.gif|\\.css|\\.js|\\.svg|\\.html|\\.xml)$";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -32,7 +38,20 @@ public class CatMonitorFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(request, response);
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+            final String path = httpServletRequest.getRequestURI();
+            if (path.startsWith("/static/") || path.startsWith("/$stapler/")
+                    || path.matches(REG)) {
+                LOGGER.info("path is no need to do CAT transaction:" + path);
+                chain.doFilter(request, response);
+                return;
+            }
+            CAT_FILTER.doFilter(request, response, chain);
+        } else {
+            chain.doFilter(request, response);
+            return;
+        }
     }
 
     @Override
