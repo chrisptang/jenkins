8c58756e448d80034878ef120263907b6ab4c271
chrisptang <chris.p.tang@gmail.com>
2021-04-07 17:41:24 +0800

    fixed

--- start_jenkins_linux.sh
+++ start_jenkins_linux.sh
@@ -1,20 +1,83 @@
 #!/bin/bash
 
+ROOT_PATH="/opt"
+
+mkdir -p ${ROOT_PATH}/logs/jenkins
+
 jenkins_war="jenkins.war"
 if [ -n "$1" ]; then
     jenkins_war=$1
 fi
 
-echo "starting jenkins with war:/opt/$jenkins_war"
 
-pid_local=`cat /logs/jenkins/jenkins.pid`
-pid_file="/logs/jenkins/jenkins.pid"
+export JDK11_FULLNAME="jdk-11.0.9_linux-x64_bin"
+export JDK11_DOWNLOAD_LINK="https://code.aliyun.com/kar/ojdk11-11.0.9/raw/master/${JDK11_FULLNAME}.tar.gz"
+export JDK8_FULLNAME="jdk-8u271-linux-x64"
+export JDK8_DOWNLOAD_LINK="https://code.aliyun.com/kar/ojdk8-8u271/raw/master/${JDK8_FULLNAME}.tar.gz"
+
+echo "current user:`whoami`"
+echo "have a check on current user's ENVs:"
+printenv
+
+is_jdk_install=1
+make_sure_jdk_installed()
+{
+    # test if java is installed
+    export JAVA_VER=$(java -version 2>&1 >/dev/null | egrep "\S+\s+version" | awk '{print $3}' | tr -d '"')
+    if test -n "${JAVA_VER}"
+    then
+        echo "java version:$JAVA_VER"
+        is_jdk_install=0
+        return $is_jdk_install
+    fi
+
+    if [ -f /opt/${JDK8_FULLNAME}/bin/java ]
+    then
+        export JAVA_HOME=/opt/${JDK8_FULLNAME}
+        export PATH=$JAVA_HOME/bin:$PATH
+        echo "${JDK8_FULLNAME} has been downlaoded before, just initial the envs."
+        echo "JAVA_HOME:${JAVA_HOME}, PATH:${PATH}"
+        is_jdk_install=0
+        return $is_jdk_install
+    fi
+
+    printf "\n\n\n========\t java is not installed yet, about to download and install it.\n"
+
+    rm -fr /opt/${JDK8_FULLNAME} && mkdir -p /opt/${JDK8_FULLNAME} && cd /opt/${JDK8_FULLNAME}
+
+    wget ${JDK8_DOWNLOAD_LINK}
+
+    tar -xvf ${JDK8_FULLNAME}.tar.gz
+
+    mv /opt/${JDK8_FULLNAME}/jdk1.8.0_271/* /opt/${JDK8_FULLNAME}
+
+    export JAVA_HOME=/opt/${JDK8_FULLNAME}
+    echo "export JAVA_HOME=/opt/${JDK8_FULLNAME}" >> ~/.bash_profile
+    export PATH=$JAVA_HOME/bin:$PATH
+    echo "export PATH=$JAVA_HOME/bin:$PATH" >> ~/.bash_profile
+
+    is_jdk_install=0
+    return $is_jdk_install
+}
+
+make_sure_jdk_installed
+
+if [ "$is_jdk_install" != "0" ]
+then
+    printf "\n=============\tunable to check JDK installed\n"
+    exit 13
+fi
+
+echo "starting jenkins with war:${ROOT_PATH}/$jenkins_war"
+
+pid_local=`cat ${ROOT_PATH}/logs/jenkins/jenkins.pid`
+pid_file="${ROOT_PATH}/logs/jenkins/jenkins.pid"
 
 cat $pid_file | xargs kill -9
 
-kill_wiat_time=60
+kill_wait_time=60
 kill_status=0
-for i in $(seq 1 1 $kill_wiat_time); do
+for i in $(seq 1 1 $kill_wait_time); do
     sleep 1
     if [ -z "`ls /proc/ |grep $pid_local`" ]; then
         kill_status=1
@@ -27,18 +90,21 @@ done
 if [[ $kill_status == 0 ]]; then
     printf "unable to kill jenkins, pid:$pid\n"
     printf "\n\n=====\t please have a check on latest app log:\n\n"
-    tail -200 /logs/jenkins/gc.log
+    tail -200 ${ROOT_PATH}/logs/jenkins/gc.log
     stop
-    exit -1
+    exit 100
 else
     echo "--- pid:$pid_local has been killed properly ---"
 fi
 
-/opt/jdk1.8.0_201/bin/java -server -Xmx2g -Xms2g -Xss256k -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m \
+jdk_full_name=`ls /opt |grep jdk-8`
+
+${ROOT_PATH}/${jdk_full_name}/bin/java -server -Xmx2g -Xms2g -Xss256k -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=512m \
 	-XX:SurvivorRatio=8 -XX:+UseConcMarkSweepGC -XX:+ExplicitGCInvokesConcurrentAndUnloadsClasses \
 	-XX:+CMSScavengeBeforeRemark -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 \
 	-XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintGCDetails -XX:+HeapDumpOnOutOfMemoryError \
-	-XX:HeapDumpPath=/logs/jenkins/heap_dump -Xloggc:/logs/jenkins/gc.log -jar /opt/$jenkins_war &>>/logs/jenkins/jenkins.log &
+	-XX:HeapDumpPath=${ROOT_PATH}/logs/jenkins/heap_dump -Xloggc:${ROOT_PATH}/logs/jenkins/gc.log -jar \
+	${ROOT_PATH}/$jenkins_war &>>${ROOT_PATH}/logs/jenkins/jenkins.log &
 
 pid_local=$!
 
@@ -50,4 +116,4 @@ ps aux|grep $pid_local
 
 sleep 5
 
-tail /logs/jenkins/jenkins.log
+tail ${ROOT_PATH}/logs/jenkins/jenkins.log
